b4525645527249cb5928caa6f3a23ec4b3f7f675
KhlebAnton <toni.farsh@icloud.com>
2021-09-08 13:15:41 +0500

    Delete MyThread

--- src/com/company/task2/Main.java
+++ src/com/company/task2/Main.java
@@ -1,5 +1,7 @@
 package com.company.task2;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.*;
 
 public class Main {
@@ -7,13 +9,21 @@ public class Main {
     public static void main(String[] args) throws ExecutionException, InterruptedException {
 
         Callable<String> myCallable = new MyCallable();
+        List<Callable<String>> callables = new ArrayList<>();
+        callables.add(myCallable);
 
+        // Создаем пул потоков фиксированного размера
         final ExecutorService threadPool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
+        // Отправляем задачу в пул потоков
         final Future<String> task = threadPool.submit(myCallable);
+        // Возврат первой успешно выполненной задачи
+        final String tasks = threadPool.invokeAny(callables);
+
+        System.out.println(task.get());
+
+        Thread.sleep(10000);
 
         threadPool.shutdown();
-        final String result = task.get();
-        System.out.println(result);
     }
 }
--- src/com/company/task2/MyCallable.java
+++ src/com/company/task2/MyCallable.java
@@ -8,24 +8,11 @@ public class MyCallable implements Callable<String> {
     }
 
     @Override
-    public String call() throws Exception {
-        ThreadGroup mainGroup = new ThreadGroup("main group");
-
-        MyThread threadOne = new MyThread(mainGroup, Thread.currentThread());
-        threadOne.setName("Я поток 1.");
-        MyThread threadTwo = new MyThread(mainGroup, Thread.currentThread());
-        threadTwo.setName("Я поток 2.");
-        MyThread threadThree = new MyThread(mainGroup, Thread.currentThread());
-        threadThree.setName("Я поток 3.");
-        MyThread threadFour = new MyThread(mainGroup, Thread.currentThread());
-        threadFour.setName("Я поток 4.");
-
-        threadOne.start();
-        threadTwo.start();
-        threadThree.start();
-        threadFour.start();
-
-        System.out.println();
-        return "Result";
+    public String call() {
+        double random = Math.random() * 5;
+        for (int i = 0; i < random; i++) {
+            System.out.println("Сообщение" + i);
+        }
+        return "Количество сообщений: " + (int) random;
     }
 }
